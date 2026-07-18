425402ff34f2e41144ac6f0ee15de70d61ef59ac
Casper Jensen <cajensen1998@gmail.com>
2026-07-18 13:08:51 +0200

    sidste commit for nu ved ikke hvad den gør helt helt præcist

--- Form1.cs
+++ Form1.cs
@@ -5,34 +5,36 @@ namespace AchievementChecklist;
 
 public partial class Form1 : Form
 {
-    
-    string path = "userData.txt";
+    private readonly string datapath = "userData.txt";
+
 
     public Form1()
     {
         InitializeComponent();
-            
+         
         var label = new Label { Text = "Enter text:", Location = new Point(10, 10), AutoSize = true };
         Controls.Add(label);
 
         var textBox = new TextBox { Location = new Point(10, 40), Width = 200 };
         Controls.Add(textBox);
 
+
         var button = new Button { Text = "Submit", Location = new Point(10, 70) };
-        // button.Click += (s, e) => MessageBox.Show("stop clicking me");
         Controls.Add(button);
 
-        // var checkBox = new CheckBox { Text = "Check me", Location = new Point(10, 100) };
-        // checkBox.CheckedChanged += (s, e) => MessageBox.Show(checkBox.Checked ? "Checked!" : "Unchecked!");
-        // Controls.Add(checkBox);
-
-        if(!File.Exists(path))
-        {
-            File.Create(path).Close();
-        }
-
         var box = new CheckedListBox { Location = new Point(10, 130), Width = 200, Height = 100 };
-        var lines = File.ReadAllLines(path);
+        Controls.Add(box);
+
+
+        var checkBox = new CheckBox { Text = "Check me", Location = new Point(10, 100) };
+        checkBox.CheckedChanged += (s, e) => MessageBox.Show(checkBox.Checked ? "Checked!" : "Unchecked!");
+        Controls.Add(checkBox);
+
+        EnsuredataFileExists();
+
+        
+        
+        var lines = File.ReadAllLines(datapath);
         box.Items.AddRange(lines);
 
         button.Click += (s, e) => 
@@ -41,8 +43,18 @@ public partial class Form1 : Form
             {
                 string text = textBox.Text;
                 box.Items.Add(text);
-                File.AppendAllText(path, text + Environment.NewLine);
+                File.AppendAllText(datapath, text + Environment.NewLine);
                 textBox.Clear();
+            }
+        };
+
+        box.ItemCheck += (s, e) =>
+        {
+            string checkedItem = box.Items[e.Index].ToString();
+
+            if (e.NewValue == CheckState.Checked)
+            {
+                box.Items[e.Index] = checkedItem + " (Completed)";
             }
         };
 
@@ -50,14 +62,32 @@ public partial class Form1 : Form
         // box.Items.AddRange(new[] { "Option 1", "Option 2", "Option 3" });
 
         
-        box.SelectedIndexChanged += (s, e) => MessageBox.Show($"Selected: {box.SelectedItem}");
-        Controls.Add(box);
 
         // textBox.TextChanged += (s, e) => label.Text = $"You entered: {textBox.Text}";
     }
 
-    private void Form1_Load(object sender, EventArgs e)
+    private void InitilizeControls()
     {
-        // This method is called when the form loads. You can add any initialization code here.
-    } 
+        var label = new Label { Text = "Enter text:", Location = new Point(10, 10), AutoSize = true };
+        Controls.Add(label);
+
+        var textBox = new TextBox { Location = new Point(10, 40), Width = 200 };
+        Controls.Add(textBox);
+
+        var button = new Button { Text = "Submit", Location = new Point(10, 70) };
+        Controls.Add(button);
+
+        var box = new CheckedListBox { Location = new Point(10, 130), Width = 200, Height = 100 };
+        Controls.Add(box);
+    }
+
+    private void EnsuredataFileExists()
+    {
+        if (!File.Exists(datapath))
+        {
+            File.Create(datapath).Close();
+        }
+    }
+
+
 }
